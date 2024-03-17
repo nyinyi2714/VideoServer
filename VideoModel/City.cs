54dd2b3f3f553a8ee96f38e6cd3c9e2fd1c4503d
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2024-03-17 02:08:43 -0700

    Initial commit

---
+++ VideoModel/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace VideoModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    public int CityId { get; set; }
+
+    [Column(TypeName = "numeric(18, 4)")]
+    public decimal Latitude { get; set; }
+
+    [Column(TypeName = "numeric(18, 4)")]
+    public decimal Longtitude { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Population { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
